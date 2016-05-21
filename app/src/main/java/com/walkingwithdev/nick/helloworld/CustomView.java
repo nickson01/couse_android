e071e45acc4ee699f9cdc924a6ed608e1fed24af
Wittavat Kanyarong <nickson_kv@hotmail.com>
2016-05-21 18:53:12 +0700

    add isBlue

--- app/src/main/java/com/walkingwithdev/nick/helloworld/CustomView.java
+++ app/src/main/java/com/walkingwithdev/nick/helloworld/CustomView.java
@@ -2,6 +2,7 @@ package com.walkingwithdev.nick.helloworld;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Build;
@@ -12,6 +13,7 @@ import android.view.View;
  * Created by Nick on 5/20/2016 AD.
  */
 public class CustomView extends View {
+    private boolean isBlue = false;
     public CustomView(Context context) {
         super(context);
         init();
@@ -20,29 +22,50 @@ public class CustomView extends View {
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
+        initWithAtts(attrs,0,0);
     }
 
     public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
+        initWithAtts(attrs,defStyleAttr,0);
     }
 
     @TargetApi(21)
     public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init();
+        initWithAtts(attrs,defStyleAttr,defStyleRes);
     }
 
     private void init() {
 
     }
+    private void  initWithAtts(AttributeSet attrs,int defStyleAttr,int defStyleRes){
+        TypedArray a = getContext().getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CustomView,
+                defStyleAttr,
+                defStyleRes);
+        try {
+            isBlue = a.getBoolean(R.styleable.CustomView_isBlue,false);
+        }finally {
+            a.recycle();
+        }
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
         Paint p = new Paint();
-        p.setColor(0xFFFF0000);
+        if (isBlue){
+            p.setColor(0xFF0000FF);
+        }else {
+            p.setColor(0xFFFF0000);
+        }
+
         canvas.drawLine(0,0,getMeasuredWidth(),getMeasuredHeight(),p);
 
     }
