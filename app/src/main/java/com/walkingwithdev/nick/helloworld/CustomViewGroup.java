12ddcd7efb56502da2c0f9c4af9338baa9e5be60
Wittavat Kanyarong <nickson_kv@hotmail.com>
2016-05-22 12:08:50 +0700

    save & restore CustomViewGroup

--- app/src/main/java/com/walkingwithdev/nick/helloworld/CustomViewGroup.java
+++ app/src/main/java/com/walkingwithdev/nick/helloworld/CustomViewGroup.java
@@ -2,7 +2,10 @@ package com.walkingwithdev.nick.helloworld;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.util.SparseArray;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
@@ -45,5 +48,59 @@ public class CustomViewGroup extends FrameLayout{
 
     public  void setButtonText(String text){
         btnHello.setText(text);
+    }
+
+    @Override
+    protected void dispatchSaveInstanceState(SparseArray<Parcelable> container) {
+       dispatchFreezeSelfOnly(container);
+    }
+
+    @Override
+    protected void dispatchRestoreInstanceState(SparseArray<Parcelable> container) {
+        dispatchThawSelfOnly(container);
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+
+        //Save chile instance
+        Bundle childrenStates = new Bundle();
+
+        for (int i = 0; i < getChildCount(); i++){
+            int id = getChildAt(i).getId();
+            if (id != 0){
+                SparseArray childrenState = new SparseArray();
+                getChildAt(i).saveHierarchyState(childrenState);
+                childrenStates.putSparseParcelableArray(String.valueOf(id),childrenState);
+            }
+        }
+        Bundle bundle = new Bundle();
+        bundle.putBundle("childrenStates",childrenStates);
+
+        //save to parcelable
+        BundleSavesState ss = new BundleSavesState(superState);
+        ss.setBundle(bundle);
+        return ss;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        BundleSavesState ss = (BundleSavesState) state;
+        super.onRestoreInstanceState(ss.getSuperState());
+
+        //restore SparseArray
+        Bundle childrenStates = ss.getBundle().getBundle("childrenStates");
+        //restore Children's state
+        for (int i = 0; i < getChildCount(); i++){
+            int id = getChildAt(i).getId();
+            if (id != 0){
+                if(childrenStates.containsKey(String.valueOf(id))){
+                    SparseArray childrenState = childrenStates.getSparseParcelableArray(String.valueOf(id));
+                    getChildAt(i).restoreHierarchyState(childrenState);
+                }
+            }
+        }
+
     }
 }
